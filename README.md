1c7c63fcb747ec4115166f71dc1fcbb098884215
Amadou Hambaliou Barry <2241112@collegemv.qc.ca>
2024-02-09 11:01:43 -0500

    sauvegarde test

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Projet de site Web scolaire
+# Projet de site Web scolaire
 
 Ce projet en est un pour permettre lacces aux etudiants au protail de notre ecole.
--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-alert("Bonjour le monde!")
+
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+var bonjour = "bonjour"
+alert(bonjour)
+
+addTaskBtn.addEventListener("click", addTask);
+
+
+function addTask() {
+    
+    const taskText = taskInput.value.trim();
+
+   
+    if (taskText !== "") {
+        
+        const listItem = document.createElement("li");
+        listItem.textContent = taskText;  
+        taskList.appendChild(listItem);
+
+        
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Supprimer";
+
+        
+        listItem.appendChild(deleteBtn);
+
+       
+        deleteBtn.addEventListener("click", () => {
+            listItem.remove();
+        });
+
+        
+        taskInput.value = "";
+    } else {
+        
+        alert("Veuillez entrer une tâche valide.");
+    }
+}
